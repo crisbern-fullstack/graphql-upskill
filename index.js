a6a51db9aed3fe1b332f52f5b45c02f49d82646c
Crisbern T. Gozun <crisbern@fullstack.ph>
2022-09-14 17:21:21 +0800

    product to category relationsip

--- index.js
+++ index.js
@@ -1,8 +1,29 @@
 const { ApolloServer, gql } = require("apollo-server");
+const { products, categories } = require("./dummyData");
 
 const typeDefs = gql`
   type Query {
     hello: [String!]!
+    products: [Product!]!
+    product(id: ID!): Product
+    categories: [Category!]!
+    category(id: ID!): Category
+  }
+
+  type Product {
+    id: ID!
+    name: String!
+    description: String!
+    quantity: Int!
+    price: Float!
+    onSale: Boolean!
+    category: Category
+  }
+
+  type Category {
+    id: ID!
+    name: String!
+    products: [Product!]!
   }
 `;
 
@@ -10,6 +31,40 @@ const resolvers = {
   Query: {
     hello: () => {
       return ["World"];
+    },
+    products: () => {
+      return products;
+    },
+    product: (parent, args, context) => {
+      const productId = args.id;
+
+      const product = products.find((product) => product.id === productId);
+
+      return product ? product : null;
+    },
+    categories: () => categories,
+    category: (parent, args, context) => {
+      const categoryId = args.id;
+
+      const category = categories.find(
+        (category) => categoryId === category.id
+      );
+
+      return category ? category : null;
+    },
+  },
+  Category: {
+    products: (parent, args, context) => {
+      const { id } = parent;
+
+      return products.filter((product) => product.categoryId === id);
+    },
+  },
+  Product: {
+    category: (parent, args, context) => {
+      const { categoryId } = parent;
+
+      return categories.find((category) => category.id === categoryId);
     },
   },
 };
